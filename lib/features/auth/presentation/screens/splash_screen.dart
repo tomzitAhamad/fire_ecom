18c64535cd708e65eb8123997b5eec3fe6bf0a8a
tomzit <barisakib6@gmail.com>
2026-04-14 01:39:00 +0600

    added firebase and localization

--- lib/features/auth/presentation/screens/splash_screen.dart
+++ lib/features/auth/presentation/screens/splash_screen.dart
@@ -1,11 +1,38 @@
+import 'package:fire_ecom/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../app/provider/language_provider.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
+  static const String routeName = '/';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
-    
+    return Scaffold(
+      appBar: AppBar(title: Text(AppLocalizations.of(context).hello)),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: FilledButton(
+              onPressed: () {
+                final provider = Provider.of<LanguageProvider>(
+                  context,
+                  listen: false,
+                );
+                if (provider.currentLocale.languageCode == 'en') {
+                  provider.setLocale(const Locale('bn'));
+                } else {
+                  provider.setLocale(const Locale('en'));
+                }
+              },
+              child: Text("Change language"),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:fire_ecom/app/ecom_app.dart';
+import 'package:fire_ecom/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   runApp(const EcomApp());
 }
